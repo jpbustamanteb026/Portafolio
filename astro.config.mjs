feaf071c0fedb1fe6f5643dc70025a84756ac9f6
Juan_Pablo <juanbermudez026@gmail.com>
2026-05-21 18:02:23 -0500

    Add menu mobile function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://jpbustamanteb026.github.io",
-  base: "/Portafolio",
+  base: import.meta.env.PROD ? "/Portafolio" : "/",
 });
