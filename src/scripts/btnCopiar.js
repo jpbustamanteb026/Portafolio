d0c550c6e94dffae09865ed08fe90772f175b649
Juan_Pablo <juanbermudez026@gmail.com>
2026-05-22 16:53:37 -0500

    fix index email icon

--- src/scripts/btnCopiar.js
+++ src/scripts/btnCopiar.js
@@ -1,24 +1,25 @@
-// Este script se ejecuta únicamente en el cliente (navegador)
-const boton = document.getElementById("copiar-correo-btn");
-const aviso = document.getElementById("mensaje-copiado");
+document.addEventListener("astro:page-load", () => {
+  const boton = document.getElementById("copiar-correo-btn");
+  const aviso = document.getElementById("mensaje-copiado");
 
-if (boton) {
-  boton.addEventListener("click", async () => {
-    const correo = boton.dataset.correo;
+  if (boton) {
+    boton.addEventListener("click", async () => {
+      const correo = boton.dataset.correo;
 
-    if (correo && aviso) {
-      try {
-        await navigator.clipboard.writeText(correo);
+      if (correo && aviso) {
+        try {
+          await navigator.clipboard.writeText(correo);
 
-        aviso.classList.add("copiado");
+          aviso.classList.add("copiado");
 
-        setTimeout(() => {
-          aviso.classList.remove("copiado");
-        }, 2000);
-      } catch (err) {
-        console.error("Error al copiar al portapapeles:", err);
-        alert("No se pudo copiar el correo");
+          setTimeout(() => {
+            aviso.classList.remove("copiado");
+          }, 2000);
+        } catch (err) {
+          console.error("Error al copiar al portapapeles:", err);
+          alert("No se pudo copiar el correo");
+        }
       }
-    }
-  });
-}
+    });
+  }
+});
