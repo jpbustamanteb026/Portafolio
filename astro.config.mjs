a906e29a37e7503a91b254313b022b8e9e84409a
Juan_Pablo <juanbermudez026@gmail.com>
2026-05-27 14:55:50 -0500

    fix vercel and githubpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from "astro/config";
 
+// Vercel define automáticamente la variable GITHUB_ACTIONS como falsa o inexistente,
+// mientras que GitHub Pages siempre corre bajo GitHub Actions.
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
   site: "https://jpbustamanteb026.github.io",
-  base: "/Portafolio",
+  // Si está en GitHub Pages usa '/Portafolio', si está en Vercel usa la raíz '/'
+  base: isGitHubPages ? "/Portafolio" : "/",
 });
